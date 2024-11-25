68148b490d95b9f0336fa71a38b41a22f29914a7
Amonios <amonios@aucegypt.edu>
2024-11-25 15:41:53 +0200

    integrated google test

--- cmake-build-debug/CTestTestfile.cmake
+++ cmake-build-debug/CTestTestfile.cmake
@@ -5,5 +5,5 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(HuffmanTests "D:/practice/FileZipper/FileZipperProject/cmake-build-debug/HuffmanTests.exe")
-set_tests_properties(HuffmanTests PROPERTIES  _BACKTRACE_TRIPLES "D:/practice/FileZipper/FileZipperProject/CMakeLists.txt;34;add_test;D:/practice/FileZipper/FileZipperProject/CMakeLists.txt;0;")
+set_tests_properties(HuffmanTests PROPERTIES  _BACKTRACE_TRIPLES "D:/practice/FileZipper/FileZipperProject/CMakeLists.txt;37;add_test;D:/practice/FileZipper/FileZipperProject/CMakeLists.txt;0;")
 subdirs("googletest")
